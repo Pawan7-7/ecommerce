4f33cb4fb95e752be85b88707960aac6482e3286
Pawan <nepsonh@gmail.com>
2022-09-26 22:07:44 +0545

    grid in mui understood

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { Grid } from "@mui/material";
+import {Box} from "@mui/material";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
   function App() {
+    const [data,setData]=useState([])
 useEffect(()=>{ axios.get('https://fakestoreapi.com/products')
 .then((response)=>{
  console.log(response.data);
+ setData(response.data)
 })},[])
+// console.log(data);
   return (
-    <div>
-     Initial Setup
-    </div>
+    <Grid container>
+     
+   {data.map((val)=>{
+    return(
+     
+ <Grid key={val.title} md={2} xs={12} sm={6} item sx={{
+  boxShadow: 3,
+ p: 1,
+ m: 1,
+ borderRadius: 2,
+ textAlign: 'center',
+ fontSize: '0.875rem',
+ fontWeight: '700', "&:hover": {
+  boxShadow: 7,
+},}}>
+         <Box  component='img' src={val.image} sx={{width:"80%"}} />
+      </Grid>
+     
+    )
+   })}
+    </Grid>
   );
 }
 
