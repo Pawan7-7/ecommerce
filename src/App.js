ca6c9f674f5c2afc92bd6ed0b046aabacef97596
Pawan <nepsonh@gmail.com>
2022-09-27 14:23:57 +0545

    react router dom setup done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import { Grid } from "@mui/material";
-import {Box} from "@mui/material";
-import axios from "axios";
-import { useEffect,useState } from "react";
+import { Home } from "./pages/Home";
+import { Route,Routes } from "react-router-dom";
+import { Cart } from "./pages/Cart";
   function App() {
-    const [data,setData]=useState([])
-useEffect(()=>{ axios.get('https://fakestoreapi.com/products')
-.then((response)=>{
- console.log(response.data);
- setData(response.data)
-})},[])
-// console.log(data);
+   
+
   return (
-   <Grid container  >
-   {
-    data.map((apiData)=>{
-      return(
-        <Grid  md={2} sm={4} xs={12} item sx={{
-          boxShadow:3,
-          m:1,
-          borderRadius:4,
-          '&:hover':{
-            boxShadow:7
-          }
-        }}>
-          <Box component='img' src={apiData.image} sx={
-            {
-              width:"80%",
-              
-            }
-          }></Box>
-        </Grid>
-      )
-    })
-   }
-   </Grid>
+<Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/cart" element={<Cart/>}/>
+
+</Routes>
+     
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Products from '../components/Products'
+export const Home = () => {
+  return (
+    <div><Products/></div>
+  )
+}
