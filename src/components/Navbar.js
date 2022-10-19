4bf231656b331334b2a06205dd09991fe99346dc
Pawan <nepsonh@gmail.com>
2022-10-19 13:35:03 +0545

    theme provider used

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {InputBase, styled, Typography} from '@mui/material'
 import { AppBar, Toolbar } from '@mui/material'
+import LocalConvenienceStoreIcon from '@mui/icons-material/LocalConvenienceStore';
+import { LocalConvenienceStore } from '@mui/icons-material';
+const NavToolBar= styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+})
+const Search =styled('div')({
+    backgroundColor:'white',
+    borderRadius: '10px',
+    width:'40%',
+    padding:'0 10px',
+    
+
+})
+
 export const Navbar = () => {
   return (
     <AppBar position='sticky'>
-        <Toolbar>
-        Navbar
-        </Toolbar>
+        <NavToolBar>
+        <Typography variant='h6' sx={{display:{xs:'none',sm:"block"}}}>.Store</Typography>
+        <LocalConvenienceStoreIcon sx={{display:{xs:'block',sm:"none"}}}></LocalConvenienceStoreIcon>
+        <Search><InputBase placeholder='Search...'></InputBase></Search>
+        </NavToolBar>
     </AppBar>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import { Provider } from 'react-redux';
 import {store} from "./components/Redux/store"
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import {theme} from "./components/theme"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemeProvider>
+
   </Provider>
  
   </BrowserRouter>
